e1b219c56089c9858aa74a6ae57aa925efc17509
JoseNSoler <josenico1212@gmail.com>
2022-04-06 13:10:40 -0500

    DELETE BACKEND pre deployment firebase

--- BACKEND/src/main/java/co/com/sofka/ferreteria/repository/interfaces/IMovimientosInventario.java
+++
@@ -1,8 +0,0 @@
-package co.com.sofka.ferreteria.repository.interfaces;
-
-import co.com.sofka.ferreteria.models.MovimientoInventario;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface IMovimientosInventario extends ReactiveMongoRepository<MovimientoInventario, String>, IRefDominioRepositorio {
-
-}
